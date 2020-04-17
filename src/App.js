61e5c7b55b424aac7295e6ccb15d1411da713c60
rafaelpiedade <rafaeldade@hotmail.com>
2020-04-17 10:43:06 -0300

    Created object LogoCollabCode

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 
 import Sobre from "./objects/Sobre";
+import LogoCollabCode from './objects/LogoCollabcode'
 
 function App() {
-  return <Sobre />;
+  return (
+    <>
+      <LogoCollabCode/>
+      <Sobre />
+    </>
+  );
 }
 
 export default App;
-
---
+++ src/objects/LogoCollabcode/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import logo from "./logo.png";
+
+const LogoCollabcode = () => {
+  return <img src={logo} alt="Logo da CollabCode" />;
+};
+
+export default LogoCollabcode;
