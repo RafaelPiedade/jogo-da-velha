354433c50c5ae0829a8bb74c2f85540ea14d031f
rafaelpiedade <rafaeldade@hotmail.com>
2020-04-23 17:06:06 -0300

    Criado o objeto IconSocial

--- src/component/ProfileUser/index.js
+++ src/component/ProfileUser/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Profile from "../../img/profile.png";
+import GitHub from "../../img/github-icon.png";
+import Facebook from '../../img/facebook-icon.png';
+import Instagran from '../../img/instagran-icon.png';
+import Linkedin from '../../img/linkedin-icon.png';
+import Twitter from '../../img/twitter-icon.png'
 import "./styles.css";
 
 import AvatarProfile from "../../objects/AvatarProfile";
+import IconSocial from "../../objects/IconSocial";
 
 const ProfileUser = () => (
   <dl className="profile-user">
@@ -11,8 +17,14 @@ const ProfileUser = () => (
     </dd>
     <dt className="title">Rafael Piedade</dt>
     <dd className="description">
-      Sou um Dev.
-      Estudo Front-End/UX e faço parte na comunidade da CollabCode.
+      Sou um Dev. Estudo Front-End/UX e faço parte na comunidade da CollabCode.
+    </dd>
+    <dd className="socials">
+      <IconSocial src={GitHub} alt="Logo da empresa GitHub" />
+      <IconSocial src={Linkedin} alt="Logo da empresa Linkedin" />
+      <IconSocial src={Facebook} alt="Logo da empresa Facebook" />
+      <IconSocial src={Twitter} alt="Logo da empresa Twitter" />
+      <IconSocial src={Instagran} alt="Logo da empresa Instagran" />
     </dd>
   </dl>
 );
---
+++ src/objects/IconSocial/index.js
@@ -0,0 +1,4 @@
+import React from "react";
+
+const IconSocial = ({src, alt}) => <img className="icon-social" src={src} alt={alt} />;
+export default IconSocial;
