905e2d22278b924c600110cfc5c148e85f0d6677
rafaelpiedade <rafaeldade@hotmail.com>
2020-04-24 08:38:50 -0300

    Removido About component, feito no desafio 52

--- src/objects/IconSocial/index.js
+++ src/objects/IconSocial/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const IconSocial = ({ action = "!#", src, alt }) => (
-  <a className="icon-social" href={action} target="_blank">
+  <a className="icon-social" href={action} target="_blank" rel="noopener noreferrer">
     <img src={src} alt={alt} />
   </a>
 );
