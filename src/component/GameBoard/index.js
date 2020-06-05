997e7095370385537225eaa0323305f85c829528
rafaelpiedade <rafaeldade@hotmail.com>
2020-06-05 15:36:47 -0300

    Prepara o history do GameBoard

--- src/component/GameBoard/index.js
+++ src/component/GameBoard/index.js
@@ -3,8 +3,39 @@ import CardWrap from "../../objects/CardGame";
 import PlayerGame from "../../objects/PlayerGame";
 import "./styles.css";
 
-const GameBoard = ({callback}) => {
+const GameBoard = ({ callback }) => {
   const [nextPlayer, setNextPlayer] = useState("x");
+  const [round, setRound] = useState(0)
+  const [history, setHistory] = useState([
+    {
+      round:0,
+      state: [
+        { id: 1, content: "" },
+        { id: 2, content: "" },
+        { id: 3, content: "" },
+        { id: 4, content: "" },
+        { id: 5, content: "" },
+        { id: 6, content: "" },
+        { id: 7, content: "" },
+        { id: 8, content: "" },
+        { id: 9, content: "" },
+      ],
+    },
+    {
+      round:1,
+      state: [
+        { id: 1, content: "" },
+        { id: 2, content: "" },
+        { id: 3, content: "" },
+        { id: 4, content: "" },
+        { id: 5, content: "x" },
+        { id: 6, content: "" },
+        { id: 7, content: "" },
+        { id: 8, content: "" },
+        { id: 9, content: "" },
+      ],
+    },
+  ]);
   const [players, setPlayers] = useState([
     { id: 1, content: "" },
     { id: 2, content: "" },
@@ -23,7 +54,7 @@ const GameBoard = ({callback}) => {
         player.id === id ? { id, content: nextPlayer } : player
       )
     );
-    callback({id, content:nextPlayer})
+    callback({ id, content: nextPlayer });
     setNextPlayer((old) => (old === "x" ? "o" : "x"));
   };
 
