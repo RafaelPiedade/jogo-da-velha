e481de1708d1d7e0a5cceb323341a236a432b71c
rafaelpiedade <rafaeldade@hotmail.com>
2020-06-08 18:38:08 -0300

    Maquina do tempo

--- src/component/GameBoard/index.js
+++ src/component/GameBoard/index.js
@@ -1,58 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
 import CardWrap from "../../objects/CardGame";
 import PlayerGame from "../../objects/PlayerGame";
 import "./styles.css";
 
-const GameBoard = ({ callback }) => {
-  const [nextPlayer, setNextPlayer] = useState("x");
-  const [lastRound, setLastRound] = useState(0);
-  const [history, setHistory] = useState([
-    {
-      round: 0,
-      state: [
-        { id: 1, content: "" },
-        { id: 2, content: "" },
-        { id: 3, content: "" },
-        { id: 4, content: "" },
-        { id: 5, content: "" },
-        { id: 6, content: "" },
-        { id: 7, content: "" },
-        { id: 8, content: "" },
-        { id: 9, content: "" },
-      ],
-    },
-  ]);
 
-  const handleClick = (id) => {
-    setHistory((old) => [
-      ...old,
-      {
-        round: lastRound + 1,
-        state: old[lastRound].state.map((player) =>
-          player.id === id ? { id, content: nextPlayer } : player
-        ),
-      },
-    ]);
-    setLastRound((old) => old + 1);
-    callback({ id, content: nextPlayer });
-    setNextPlayer((old) => (old === "x" ? "o" : "x"));
-  };
-
-  return (
-    <CardWrap>
-      <ul className="game-board">
-        {history[lastRound].state.map((player) => (
-          <li
-            className="item"
-            key={player.id}
-            onClick={() => player.content === "" && handleClick(player.id)}
-          >
-            <PlayerGame id={player.id} content={player.content} />
-          </li>
-        ))}
-      </ul>
-    </CardWrap>
-  );
-};
+const GameBoard = ({ historyGame, onClick, lastRound }) => (
+  <CardWrap>
+    <ul className="game-board">
+      {historyGame[lastRound].state.map((player) => (
+        <li
+          className="item"
+          key={player.id}
+          onClick={() => player.content === "" && onClick(player.id)}
+        >
+          <PlayerGame id={player.id} content={player.content} />
+        </li>
+      ))}
+    </ul>
+  </CardWrap>
+);
 
 export default GameBoard;
--- src/objects/WrapperBoardHistory/index.js
+++ src/objects/WrapperBoardHistory/index.js
@@ -6,17 +6,58 @@ import InputCheckBox from "../InputCheckBox";
 const WrapperBoardHistory = () => {
   const [history, setHistory] = useState(["Start"]);
   const [active, setActive] = useState(false);
+  const [lastRound, setLastRound] = useState(0);
+  const [nextPlayer, setNextPlayer] = useState("x");
+  const [historyGame, setHistoryGame] = useState([
+    {
+      round: 0,
+      state: [
+        { id: 1, content: "" },
+        { id: 2, content: "" },
+        { id: 3, content: "" },
+        { id: 4, content: "" },
+        { id: 5, content: "" },
+        { id: 6, content: "" },
+        { id: 7, content: "" },
+        { id: 8, content: "" },
+        { id: 9, content: "" },
+      ],
+    },
+  ]);
 
   const addHistory = (player) =>
     setHistory((old) => [...old, `Adicionou ${player.content.toUpperCase()}`]);
 
   const showHideHistory = () => setActive((old) => !!!old);
 
-  const changeHistory = (key) => setHistory((old) => old.slice(0, key + 1));
+  const changeHistory = (key) => {
+    setLastRound(key);
+    setHistory((old) => old.slice(0, key + 1));
+    setHistoryGame((old) => old.slice(0, key + 1));
+  };
+
+  const handleClick = (id) => {
+    setHistoryGame((old) => [
+      ...old,
+      {
+        round: lastRound + 1,
+        state: old[lastRound].state.map((player) =>
+          player.id === id ? { id, content: nextPlayer } : player
+        ),
+      },
+    ]);
+    setLastRound((old) => old + 1);
+    addHistory({ id, content: nextPlayer });
+    setNextPlayer((old) => (old === "x" ? "o" : "x"));
+  };
 
   return (
     <div className={`wrapper-boardHistory ${active && "-active"}`}>
-      <GameBoard callback={addHistory} />
+      <GameBoard
+        historyGame={historyGame}
+        onClick={handleClick}
+        lastRound={lastRound}
+      />
       <InputCheckBox
         onClick={showHideHistory}
         id="show"
