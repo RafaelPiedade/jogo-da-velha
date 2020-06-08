66eb7762da7d6f9d624b14f78c68f190758c3304
rafaelpiedade <rafaeldade@hotmail.com>
2020-06-08 18:13:58 -0300

    Move os childrens para o WrapperBoardHistory

--- src/App.js
+++ src/App.js
@@ -2,44 +2,22 @@ import React, { useState } from "react";
 import "./App.css";
 
 import HeaderGame from "./component/HeaderGame";
-import GameBoard from "./component/GameBoard";
-import InputCheckBox from "./objects/InputCheckBox";
 import LayerDark from "./objects/LayerDark";
 
 import HeaderInternal from "./component/HeaderInternal";
 import ProfileUser from "./component/ProfileUser";
-import HistoryGame from "./component/HistoryGame";
 import WrapperBoardHistory from "./objects/WrapperBoardHistory";
 
 const App = () => {
-  const [active, setActive] = useState(false);
   const [activeAbout, setActiveAbout] = useState("");
   const handleClickAdd = () => setActiveAbout("-active");
   const handleClickRemove = () => setActiveAbout("");
 
-  const [history, setHistory] = useState(["Start"]);
-  const addHistory = (player) => {
-    setHistory((old) => [...old, `Adicionou ${player.content.toUpperCase()}`]);
-  };
-  const changeHistory = (key) => {
-    setHistory((old) => old.slice(0, key + 1));
-    console.log(key);
-  };
-
-  const showHideHistory = () => setActive((old) => !!!old);
   return (
     <main id="main" className="app">
       <HeaderGame onClick={handleClickAdd} />
-      <WrapperBoardHistory active={active}>
-        <GameBoard callback={addHistory} />
-        <InputCheckBox
-          onClick={showHideHistory}
-          id="show"
-          value="show"
-          content="Mostrar Eventos"
-        />
-        <HistoryGame history={history} onClick={changeHistory} />
-      </WrapperBoardHistory>
+      <WrapperBoardHistory />
+
       <LayerDark className={activeAbout}>
         <HeaderInternal onClick={handleClickRemove} />
         <ProfileUser />
--- src/objects/WrapperBoardHistory/index.js
+++ src/objects/WrapperBoardHistory/index.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import HistoryGame from "../../component/HistoryGame";
+import GameBoard from "../../component/GameBoard";
+import InputCheckBox from "../InputCheckBox";
 
-const WrapperBoardHistory = ({ children, active = false }) => (
-  <div className={`wrapper-boardHistory ${active && '-active'}`}>{children}</div>
-);
+const WrapperBoardHistory = () => {
+  const [history, setHistory] = useState(["Start"]);
+  const [active, setActive] = useState(false);
+
+  const addHistory = (player) =>
+    setHistory((old) => [...old, `Adicionou ${player.content.toUpperCase()}`]);
+
+  const showHideHistory = () => setActive((old) => !!!old);
+
+  const changeHistory = (key) => setHistory((old) => old.slice(0, key + 1));
+
+  return (
+    <div className={`wrapper-boardHistory ${active && "-active"}`}>
+      <GameBoard callback={addHistory} />
+      <InputCheckBox
+        onClick={showHideHistory}
+        id="show"
+        value="show"
+        content="Mostrar Eventos"
+      />
+      <HistoryGame history={history} onClick={changeHistory} />
+    </div>
+  );
+};
 
 export default WrapperBoardHistory;
